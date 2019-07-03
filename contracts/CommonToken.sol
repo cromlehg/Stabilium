a89ccbec871f2386f37901088040e9cb8eea9d0c
Alexander Strakh <strakh@ispras.ru>
2019-07-03 14:28:10 +0300

    Token first implementation

---
+++ contracts/CommonToken.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.0;
+
+import "./token/ERC20/ERC20.sol";
+import "./token/ERC20/ERC20Detailed.sol";
+import "./ownership/Ownable.sol";
+
+/**
+ * @title CommonToken
+ * @dev Very simple ERC20 Token, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+contract CommonToken is ERC20, ERC20Detailed, Ownable {
+
+    bool public lockPeriodFinished = false;
+
+    mapping(address => bool) public lockFilter;
+
+    /**
+     * @dev Constructor that gives operator all of existing tokens.
+     */
+    constructor (address operator, string memory name, string memory ticker, uint256 count) public ERC20Detailed(name, ticker, 18) {
+        _mint(operator, count * (10 ** uint256(decimals())));
+        transferOwnership(operator);
+    }
+
+    modifier notLocked(address actor) {
+	require(lockPeriodFinished || actor == owner() || lockFilter[actor], "You are not allowed to move tokens.");
+	_;
+    }
+
+    function finishLockPeriod() public {
+      require(!lockPeriodFinished, "Lock period already finished.");
+      lockPeriodFinished = true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) notLocked(sender) public returns (bool) {
+	return super.transferFrom(sender, recipient, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) notLocked(msg.sender) public returns (bool) {
+	return super.transferFrom(msg.sender, recipient, amount);        
+    }
+
+}
--- contracts/TargetToken.sol
+++ contracts/TargetToken.sol
@@ -1,22 +1,12 @@
 pragma solidity ^0.5.0;
 
-import "../token/ERC20/ERC20.sol";
-import "../token/ERC20/ERC20Detailed.sol";
-import "../ownership/Ownable.sol";
+import "./CommonToken.sol";
 
 /**
  * @title TargetToken
- * @dev Very simple ERC20 Token, where all tokens are pre-assigned to the creator.
- * Note they can later distribute these tokens as they wish using `transfer` and other
- * `ERC20` functions.
  */
-contract TargetToken is ERC20, ERC20Detailed, Ownable {
+contract TargetToken is CommonToken {
 
-    /**
-     * @dev Constructor that gives msg.sender all of existing tokens.
-     */
-    constructor (address operator, string name, string ticker, uint256 count) public ERC20Detailed(name, ticker, 18) {
-        _mint(operator, count * (10 ** uint256(decimals())));
-	transferOwnership(operator);
+    constructor (address operator, string memory name, string memory ticker, uint256 count) public CommonToken(0xEA15Adb66DC92a4BbCcC8Bf32fd25E2e86a2A770, "Stabilium", "STB", 5000000000) {
     }
 }
